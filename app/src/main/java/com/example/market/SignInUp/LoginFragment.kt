2f71401e2cf8fe4ff6af97c2d74ddb9585e09b73
estherP23 <123536492+estherP23@users.noreply.github.com>
2023-12-06 02:31:44 +0900

    Update LoginFragment.kt

--- app/src/main/java/com/example/market/SignInUp/LoginFragment.kt
+++ app/src/main/java/com/example/market/SignInUp/LoginFragment.kt
@@ -1,56 +1,38 @@
 package com.example.market.SignInUp
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.market.BottomNavigationActivity
+import androidx.navigation.fragment.findNavController
 import com.example.market.R
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 
-class LoginFragment : Fragment() {
-    private lateinit var auth: FirebaseAuth
-    private lateinit var db: FirebaseFirestore
+class Initial_fragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //매개변수로 받아온 inflater 객체의 inflate 메소드를 사용해 fragment_signUp 레이아웃 파일을
-        //실제 View 객체로 전환해줌.
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.first_fragment, container, false)
 
-        //로그인을 위한 auth 와 저장이 되어있는지 확인하기위한 데이터베이스 firestore 객체를 가져온다.
-        auth = FirebaseAuth.getInstance()
-        db = FirebaseFirestore.getInstance()
+        // NavController를 가져옵니다. StartActivity 에서 nav_host_fragment를 지정해뒀고
+        // nav_graph에서 initialFragment를 설정해뒀기에 findNavController를 이용해서
+        //navController를 가져올 수 있고 이걸 이용해서 navigate롤 화면 전환을 수행한다.
+        val navController = findNavController()
 
-        val emailEditText = view.findViewById<EditText>(R.id.emailEditText)
-        val passwordEditText = view.findViewById<EditText>(R.id.PasswordEditText)
-        val loginBtn = view.findViewById<Button>(R.id.loginBtn)
+        val btnLogin = view.findViewById<Button>(R.id.btn_login)
+        val btnSignUp = view.findViewById<Button>(R.id.btn_sign_up)
 
-        //로그인 버튼을 눌렀을 때 행하는 동작 구현
-        loginBtn.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
+        btnLogin.setOnClickListener {
+            // 로그인 버튼을 눌렀을 때 로그인 화면으로 이동하는 코드, 액션 설정해둠!
+            navController.navigate(R.id.action_initialFragment_to_loginFragment)
+        }
 
-            //auth의 이 메소드를 이용해서 이메일과 패스워드를 주고 로그인을 시도
-            auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        // 로그인 성공, 메인 화면으로 이동
-                        val intent = Intent(context, BottomNavigationActivity::class.java)
-                        startActivity(intent)
-                    } else {
-                        // 로그인 실패, 오류 메시지 표시
-                        Toast.makeText(context, "아이디/비밀번호 오류", Toast.LENGTH_SHORT).show()
-                    }
-                }
+        btnSignUp.setOnClickListener {
+            // 회원가입 버튼을 눌렀을 때 회원가입 화면으로 이동하는 코드
+            navController.navigate(R.id.action_initialFragment_to_signUpFragment)
         }
 
         return view
