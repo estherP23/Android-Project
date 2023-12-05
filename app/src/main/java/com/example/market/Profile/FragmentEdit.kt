dfebae516d5db924b6343ac88d85cd20e5b30bfd
dain0l <123536537+dain0l@users.noreply.github.com>
2023-12-06 01:05:40 +0900

    FragmentEdit.kt

--- app/src/main/java/com/example/market/Profile/FragmentEdit.kt
+++ app/src/main/java/com/example/market/Profile/FragmentEdit.kt
@@ -10,12 +10,12 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.example.market.ProductDetail
+import com.example.market.DetailFragmen
 import com.example.market.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
-class EditFragment : Fragment() {
+class FragmentEdit : Fragment() {
 
     // Firebase Firestore 인스턴스 생성
     private lateinit var db: FirebaseFirestore
@@ -25,7 +25,7 @@ class EditFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // fragment_edit_post 레이아웃을 인플레이트하여 뷰 생성
-        val view = inflater.inflate(R.layout.fragment_edit_post, container, false)
+        val view = inflater.inflate(R.layout.product_edit_fragment, container, false)
 
         // 전달된 productId를 가져오기
         val productId = arguments?.getString("productId")
@@ -37,28 +37,30 @@ class EditFragment : Fragment() {
 
             // 뷰 요소 초기화
             val titleTextView = view.findViewById<TextView>(R.id.titleTextView)
-            val titleEditText = view.findViewById<EditText>(R.id.titleEditText)
+
             val contentTextView = view.findViewById<TextView>(R.id.contentTextView)
-            val contentEditText = view.findViewById<EditText>(R.id.contentEditText)
-            val priceTextView = view.findViewById<TextView>(R.id.priceTextView)
-            val priceEditText = view.findViewById<EditText>(R.id.priceEditText)
+
+            val priceText = view.findViewById<TextView>(R.id.priceTextView)
+            val editPriceText = view.findViewById<EditText>(R.id.priceEditText)
             val sellerTextView = view.findViewById<TextView>(R.id.sellerTextView)
-            val isAvailableCheckBox = view.findViewById<CheckBox>(R.id.isAvailableCheckBox)
+            val availableCheckBox = view.findViewById<CheckBox>(R.id.isAvailableCheckBox)
             val editButton = view.findViewById<Button>(R.id.editButton)
+            val titleEditText = view.findViewById<EditText>(R.id.titleEditText)
+            val contentEditText = view.findViewById<EditText>(R.id.contentEditText)
 
             // Firebase Firestore에서 해당 productId의 데이터 가져오기
             db.collection("posts")
                 .document(productId)
                 .get()
                 .addOnSuccessListener { documentSnapshot ->
-                    val product = documentSnapshot.toObject(ProductDetail::class.java)
+                    val product = documentSnapshot.toObject(DetailFragmen::class.java)
                     if (product != null) {
                         // 제품 정보를 뷰에 설정
                         titleTextView.text = product.title
                         contentTextView.text = product.content
-                        priceTextView.text = product.price.toString()
+                        priceText.text = product.price.toString()
                         sellerTextView.text = product.seller
-                        isAvailableCheckBox.isChecked = product.sell
+                        availableCheckBox.isChecked = product.sell
 
                         // 수정 버튼 클릭 리스너 설정
                         editButton.setOnClickListener {
@@ -66,26 +68,26 @@ class EditFragment : Fragment() {
                                 // 수정하기 버튼 클릭 시, 편집 가능한 뷰로 변경
                                 titleEditText.setText(titleTextView.text.toString())
                                 contentEditText.setText(contentTextView.text.toString())
-                                priceEditText.setText(priceTextView.text.toString())
+                                editPriceText.setText(priceText.text.toString())
 
                                 // 뷰 가시성 설정
                                 titleEditText.visibility = View.VISIBLE
                                 contentEditText.visibility = View.VISIBLE
-                                priceEditText.visibility = View.VISIBLE
-                                isAvailableCheckBox.visibility = View.VISIBLE
+                                editPriceText.visibility = View.VISIBLE
+                                availableCheckBox.visibility = View.VISIBLE
                                 sellerTextView.visibility = View.VISIBLE
 
                                 titleTextView.visibility = View.GONE
                                 contentTextView.visibility = View.GONE
-                                priceTextView.visibility = View.GONE
+                                priceText.visibility = View.GONE
 
                                 editButton.text = "수정완료"
                             } else if (editButton.text == "수정완료") {
                                 // 수정완료 버튼 클릭 시, 입력된 데이터로 게시물 업데이트
                                 val title = titleEditText.text.toString()
                                 val content = contentEditText.text.toString()
-                                val priceString = priceEditText.text.toString()
-                                val isAvailable = isAvailableCheckBox.isChecked
+                                val priceString = editPriceText.text.toString()
+                                val isAvailable = availableCheckBox.isChecked
                                 val price = priceString.toIntOrNull()
 
                                 // 입력 값 유효성 검사
@@ -118,16 +120,16 @@ class EditFragment : Fragment() {
 
                                         titleEditText.visibility = View.GONE
                                         contentEditText.visibility = View.GONE
-                                        priceEditText.visibility = View.GONE
-                                        isAvailableCheckBox.visibility = View.GONE
+                                        editPriceText.visibility = View.GONE
+                                        availableCheckBox.visibility = View.GONE
 
                                         titleTextView.visibility = View.VISIBLE
                                         contentTextView.visibility = View.VISIBLE
-                                        priceTextView.visibility = View.VISIBLE
+                                        priceText.visibility = View.VISIBLE
 
                                         titleTextView.text = title
                                         contentTextView.text = content
-                                        priceTextView.text = price.toString()
+                                        priceText.text = price.toString()
 
                                         editButton.text = "수정하기"
                                     }
